fef8f952ec0b2f87562c24cc898cf7e73827c917
Travis Johnston & Carlo De Gregorio <student@epicodus.com>
2015-08-10 11:16:53 -0700

    set up /triangle routing

--- app.rb
+++ app.rb
@@ -4,3 +4,8 @@ require('./lib/triangle')
 get('/') do
   erb(:home)
 end
+
+get('/triangle') do
+  @results = Triangle.new(params['side_a'], params['side_b'], params['side_c'])
+  erb(:triangle)
+end
